47ad1bbb997dfb2e3a0f3af76c8f21b6a0e55000
Иван Запара <truthpruthbg@gmail.com>
2026-04-06 19:42:49 +0300

    Фикс корсов

---
+++ src/main/kotlin/ru/kinoko/kinchat/config/CorsConfig.kt
@@ -0,0 +1,32 @@
+package ru.kinoko.kinchat.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.applyPermitDefaultValues()
+        configuration.allowedOriginPatterns = listOf(
+            "http://localhost:5173",
+            "https://kinoko.su",
+        )
+        configuration.allowedMethods = listOf("*")
+        configuration.allowedHeaders = listOf("*")
+        configuration.allowCredentials = true
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+
+    @Bean
+    fun corsFilter() = CorsFilter(corsConfigurationSource())
+}
